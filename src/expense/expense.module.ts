735c7e1b5120cfe47fd19e8e3ba4300de3880493
= <=>
2024-03-13 10:55:38 +0500

    expense validation on save

--- src/expense/expense.module.ts
+++ src/expense/expense.module.ts
@@ -4,12 +4,10 @@ import { ExpenseController } from './expense.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Expense } from './entities/expense.entity';
 import { User } from 'src/users/entities/user.entity';
+import { Income } from 'src/income/entities/income.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Expense]),
-    TypeOrmModule.forFeature([User]),
-  ],
+  imports: [TypeOrmModule.forFeature([Expense, User, Income])],
   controllers: [ExpenseController],
   providers: [ExpenseService],
 })
--- src/expense/expense.service.ts
+++ src/expense/expense.service.ts
@@ -1,21 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateExpenseDto } from './dto/create-expense.dto';
 import { UpdateExpenseDto } from './dto/update-expense.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { And, LessThan, MoreThan, Repository } from 'typeorm';
 import { Expense } from './entities/expense.entity';
 import { User } from 'src/users/entities/user.entity';
+import { Income } from 'src/income/entities/income.entity';
 
 @Injectable()
 export class ExpenseService {
   constructor(
     @InjectRepository(Expense)
     private readonly expenseRepository: Repository<Expense>,
+    @InjectRepository(Income)
+    private readonly incomeRepository: Repository<Income>,
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
   ) {}
   async create(userId: number, createExpenseDto: CreateExpenseDto) {
     const user = await this.userRepository.findOne({ where: { id: userId } });
+    // add all incomes of uer
+    const incomes = await this.incomeRepository.find({
+      where: { user: { id: userId } },
+    });
+    const totalIncome = incomes.reduce((acc, item) => {
+      return (acc += item.amount);
+    }, 0);
+    // add all expenses of user
+    const expenses = await this.expenseRepository.find({
+      where: { user: { id: userId } },
+    });
+    const totalExpense = expenses.reduce((acc, item) => {
+      return (acc += item.amount);
+    }, 0);
+    // balance = income - expenses of user
+    const availableBalance = totalIncome - totalExpense;
+    if (createExpenseDto.amount > availableBalance) {
+      throw new BadRequestException('Insufficient Funds');
+    }
     const expense = new Expense();
     expense.type = createExpenseDto.type;
     expense.amount = createExpenseDto.amount;
